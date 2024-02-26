16a6c1262f639f06fae74949bba1ea5dbe8bf8bd
= <=>
2024-02-26 19:50:51 +0100

    upstream

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,8 +1,10 @@
 from typing import Dict, Type, Any, Tuple
 from algorithms.base_algorithm import BaseRLAlgorithm
 from algorithms.random import RandomAlgorithm
+from algorithms.r2 import Random2
 
 
 algo_name_to_AlgoClass: Dict[str, Type[BaseRLAlgorithm]] = {
     "Random": RandomAlgorithm,
+    "Random2": Random2,
 }
