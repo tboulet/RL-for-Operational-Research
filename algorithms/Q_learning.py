114276ec52894ebb7619b64032ad48eb113eb64f
tboulet <timothe.boulet0@gmail.com>
2024-03-19 12:19:58 +0100

    renaming

--- algorithms/Q_learning.py
+++ algorithms/Q_learning.py
@@ -2,9 +2,11 @@
 
 import wandb
 from tensorboardX import SummaryWriter
+
 # Config system
 import hydra
 from omegaconf import OmegaConf, DictConfig
+
 # Utils
 from tqdm import tqdm
 import datetime
@@ -24,7 +26,8 @@ from src.typing import State, Action
 from .base_algorithm import BaseRLAlgorithm
 from itertools import product
 
-class Q_learning(BaseRLAlgorithm):
+
+class Q_learning_v0(BaseRLAlgorithm):
 
     def __init__(self, config: Dict):
         super().__init__(config)
@@ -35,48 +38,53 @@ class Q_learning(BaseRLAlgorithm):
         self.min_Q = self.config["min_Q_init"]
         self.max_Q = self.config["max_Q_init"]
 
-
-
-
-    def act(self, state: State, available_actions: List[Action], is_eval: bool = False) -> Action:
+    def act(
+        self, state: State, available_actions: List[Action], is_eval: bool = False
+    ) -> Action:
         if str(state) not in self.Q:
             if self.value is None:
-                self.Q[str(state)] = {action: np.random.uniform(self.min_Q,self.max_Q) \
-                                for action in available_actions}
+                self.Q[str(state)] = {
+                    action: np.random.uniform(self.min_Q, self.max_Q)
+                    for action in available_actions
+                }
             else:
-                self.Q[str(state)] = {action: self.value for action in available_actions}
+                self.Q[str(state)] = {
+                    action: self.value for action in available_actions
+                }
         if np.random.uniform() < self.eps:
             return random.choice(available_actions)
         Q_state = self.Q[str(state)]
-        best_val = -np.inf 
+        best_val = -np.inf
         best_act = []
         for action in available_actions:
             if Q_state[action] > best_val:
                 best_act = [action]
-                best_val = Q_state[action] 
+                best_val = Q_state[action]
             if Q_state[action] == best_val:
                 best_act.append(action)
         return random.choice(best_act)
-        
-    def update(self, state: State, action: Action, reward: float, next_state: State, done: bool) -> Dict[str, float]:
+
+    def update(
+        self, state: State, action: Action, reward: float, next_state: State, done: bool
+    ) -> Dict[str, float]:
         if done:
-            self.Q[str(state)][action] = (1-self.alpha)*self.Q[str(state)][action] + self.alpha*reward
+            self.Q[str(state)][action] = (1 - self.alpha) * self.Q[str(state)][
+                action
+            ] + self.alpha * reward
             self.value = None
         else:
             if str(next_state) not in self.Q:
-                self.value = np.random.uniform(self.min_Q,self.max_Q)
+                self.value = np.random.uniform(self.min_Q, self.max_Q)
             else:
                 self.value = max(self.Q[str(next_state)].values())
-            self.Q[str(state)][action] = (1-self.alpha)*self.Q[str(state)][action] + self.alpha*(reward + self.value)
+            self.Q[str(state)][action] = (1 - self.alpha) * self.Q[str(state)][
+                action
+            ] + self.alpha * (reward + self.value)
         return {}
         return {
             **{f"Q(s={s}, a={a})": self.Q[s][a] for s in self.Q for a in self.Q[s]},
-            **{f"1(A={a} in S={state})": int(a == action) for a in self.Q[str(state)].keys()},
+            **{
+                f"1(A={a} in S={state})": int(a == action)
+                for a in self.Q[str(state)].keys()
+            },
         }
-        
-            
-
-
-
-
-    
--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,24 +1,26 @@
 from typing import Dict, Type, Any, Tuple
-from algorithms.SARSA import SARSA
+from algorithms.SARSA import SARSA_v0
 from algorithms.base_algorithm import BaseRLAlgorithm
-from algorithms.general_policy_iterator_algorithms.double_q_learning import DoubleQ_Learning
+from algorithms.general_policy_iterator_algorithms.double_q_learning import (
+    DoubleQ_Learning,
+)
 from algorithms.general_policy_iterator_algorithms.monte_carlo import MonteCarlo
 from algorithms.general_policy_iterator_algorithms.n_step_sarsa import n_step_SARSA
-from algorithms.general_policy_iterator_algorithms.q_learning import Q_LearningGPI
-from algorithms.general_policy_iterator_algorithms.sarsa import SARSA_GPI
+from algorithms.general_policy_iterator_algorithms.q_learning import Q_Learning
+from algorithms.general_policy_iterator_algorithms.sarsa import SARSA
 from algorithms.random import RandomAlgorithm
 from algorithms.r2 import Random2
-from algorithms.Q_learning import Q_learning
+from algorithms.Q_learning import Q_learning_v0
 
 
 algo_name_to_AlgoClass: Dict[str, Type[BaseRLAlgorithm]] = {
     "Random": RandomAlgorithm,
     "Random2": Random2,
-    "Q_learning": Q_learning,
+    "Q-Learning v0": Q_learning_v0,
     "Double Q-Learning": DoubleQ_Learning,
-    "SARSA": SARSA,
+    "SARSA v0": SARSA_v0,
     "Monte Carlo": MonteCarlo,
-    "SARSA GPI": SARSA_GPI,
+    "SARSA": SARSA,
     "n-step SARSA": n_step_SARSA,
-    "Q_Learning GPI": Q_LearningGPI,
+    "Q-Learning": Q_Learning,
 }
