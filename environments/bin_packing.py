d8e8e958a5dfc7107a59c929fe2cef7dbc629b5d
pierre Prevot <pierre.prevothelloco@student-cs.fr>
2024-04-01 23:04:25 +0200

    correction petite erreur sur la creation des objets (frontiere des objets)

--- environments/bin_packing.py
+++ environments/bin_packing.py
@@ -82,7 +82,7 @@ class BinPacking(BaseOREnvironment):
         self.objects = np.array(objects).round(2)
         np.random.shuffle(self.objects)
         self.n = len(self.objects)
-        
+
         assert ( np.isclose(np.sum(self.objects), self.capacity * self.nb_bins_optimal, rtol=1e-5)
         ), "Sum of object sizes must be equal to capacity * nb_bins_optimal"
 
@@ -93,6 +93,7 @@ class BinPacking(BaseOREnvironment):
         # print("\n Reset called\n")
         self.bins = []
         self.current_index = 0
+        self.reward_render = 0 #Used for rendering only
 
         return repr(self.bins), {}
 
@@ -117,6 +118,7 @@ class BinPacking(BaseOREnvironment):
         if action == len(self.bins):
             self.bins.append(self.capacity)
             reward = -1.0
+            self.reward_render += reward
         self.bins[action] -= self.objects[self.current_index]
         self.current_index += 1
         if self.current_index >= len(self.objects):
@@ -143,7 +145,8 @@ class BinPacking(BaseOREnvironment):
         all_objects = list(deepcopy(self.objects))
         all_objects.insert(self.current_index, "X")
         print(
-            f"    Bins: {self.get_format_state()}\n Objects: {all_objects}\n (X=current index)"
+            f"    Bins: {self.get_format_state()}\n Objects: {all_objects}\n (X=current index)\
+                \n possible actions: {self.get_available_actions(self.get_format_state())}, current reward {self.reward_render} \n"
         )
 
     def get_optimal_reward(self) -> float:
@@ -172,6 +175,7 @@ class BinPacking(BaseOREnvironment):
         ).round(2)
         object_borders = np.insert(object_borders, 0, 0)
         object_borders = np.insert(object_borders, 0, self.capacity)
+        object_borders = np.unique(object_borders)
         object_borders.sort()
         object_sizes = np.diff(object_borders)
         object_sizes[-1] = self.capacity - sum(object_sizes[:-1])
@@ -179,6 +183,7 @@ class BinPacking(BaseOREnvironment):
         assert (
             np.isclose(np.sum(object_sizes), self.capacity, rtol=1e-5),
         ), "Sum of object sizes must be equal to capacity"
+        assert 0 not in object_sizes, "No object size can be equal to 0"
 
         return np.around(object_sizes, decimals=2).tolist()
     
